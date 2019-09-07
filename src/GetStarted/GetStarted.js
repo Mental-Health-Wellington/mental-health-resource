03c79dae2587750218b638dfed05da79d7ea2df8
Andy Hansen <ahansen.nz@gmail.com>
2019-09-07 17:03:17 +1200

    refactor: improve get started

--- src/GetStarted/GetStarted.js
+++ src/GetStarted/GetStarted.js
@@ -3,7 +3,7 @@ import './GetStarted.scss'
 
 export default function GetStarted({ onClick }) {
   return (
-    <section>
+    <section className="get-started">
       <h2>I want to take the next step</h2>
       <button onClick={onClick}>Begin your mental health journey</button>
     </section>
