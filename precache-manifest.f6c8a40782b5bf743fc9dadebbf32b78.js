eb51db0a51ea4eed55862e452e0bfe369b47e2cf
Andy Hansen <ahansen.nz@gmail.com>
2019-09-07 17:31:52 +1200

    Updates

--- precache-manifest.f6c8a40782b5bf743fc9dadebbf32b78.js
+++ precache-manifest.f6c8a40782b5bf743fc9dadebbf32b78.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ba40fb598bf265b6e944d56c5cca10d3",
+    "revision": "61e839470f958e7e760763401385bf5e",
     "url": "/mental-health-resource/index.html"
   },
   {
-    "revision": "470a93b5b933cb656f16",
-    "url": "/mental-health-resource/static/css/main.498d6cff.chunk.css"
+    "revision": "16ad93991db8a6e45ff4",
+    "url": "/mental-health-resource/static/css/main.fd4ae221.chunk.css"
   },
   {
     "revision": "7757f55075b839fcded8",
     "url": "/mental-health-resource/static/js/2.1b94c57a.chunk.js"
   },
   {
-    "revision": "470a93b5b933cb656f16",
-    "url": "/mental-health-resource/static/js/main.b4afd1a1.chunk.js"
+    "revision": "16ad93991db8a6e45ff4",
+    "url": "/mental-health-resource/static/js/main.b81dfd72.chunk.js"
   },
   {
     "revision": "c37c05ae3ad4744236fa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mental-health-resource/precache-manifest.6ef4fde5910c43c5bdc54134aaa6e23f.js"
+  "/mental-health-resource/precache-manifest.f6c8a40782b5bf743fc9dadebbf32b78.js"
 );
 
 self.addEventListener('message', (event) => {
