62a873fb907dd1573387d122604289f583f9ac9c
Andy Hansen <ahansen.nz@gmail.com>
2019-09-07 17:31:31 +1200

    feat: cute buttons

--- src/GetStarted/GetStarted.js
+++ src/GetStarted/GetStarted.js
@@ -5,7 +5,7 @@ export default function GetStarted({ onClick }) {
   return (
     <section className="get-started">
       <h2>I want to take the next step</h2>
-      <button onClick={onClick}>Begin your mental health journey</button>
+      <button style={{ width: 'auto'}} onClick={onClick}>Begin your mental health journey</button>
     </section>
   )
 }
--- src/InputScreen/InputScreen.js
+++ src/InputScreen/InputScreen.js
@@ -5,9 +5,11 @@ export default function InputScreen({ question, values, onClick }) {
   return (
     <section className="input-screen">
       <h2>{question}</h2>
-      {values.map((value) =>
-        <button key={value} onClick={() => onClick(value)}>{value}</button>
-      )}
+      <div className="button-container">
+        {values.map((value) =>
+          <button key={value} onClick={() => onClick(value)}>{value}</button>
+        )}
+      </div>
     </section>
   )
 }
