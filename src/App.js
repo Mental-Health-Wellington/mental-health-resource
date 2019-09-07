55de31d03bc5c4b18fe78263f78f3dbd891ca0c8
Andy Hansen <ahansen.nz@gmail.com>
2019-09-07 12:16:45 +1200

    feat: super basic user input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+
+const sexValues = [
+  "Male", "Female"
+]
+
+const ageValues = [
+  "15–24", "25-34", "35-44"
+]
 
 function App() {
+  const [sex, setSex] = useState(null)
+  const [age, setAge] = useState(null)
+
+  function currentPage() {
+    if (!sex) {
+      return (
+        <section>
+          <h2>Are you a</h2>
+          {sexValues.map((sex) => 
+            <button onClick={() => setSex(sex)}>{sex}</button>
+          )}
+        </section>
+      )
+    } else if (!age) {
+      return (
+        <section>
+          <h2>How old are you?</h2>
+          {ageValues.map((age) => 
+            <button onClick={() => setAge(age)}>{age}</button>
+          )}
+        </section>
+      )
+    } else {
+      return (<p>{sex} {age}</p>)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mental Health Awareness Tool</h1>
+        {currentPage()}
       </header>
     </div>
   );
